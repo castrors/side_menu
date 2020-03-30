37ba04e424c2f348bbb720f242b4f29a41cef815
Rodrigo Castro <rodrigo.castro@easynvest.com.br>
2020-03-30 11:28:07 -0300

    first commit

---
+++ example/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:side_menu/side_menu.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Navigation Drawer Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  var _items = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Side Menu",
+          ),
+        ),
+        //drawer: CollapsingNavigationDrawer(),
+        body: Stack(
+          children: <Widget>[
+            Container(color: Colors.grey),
+            SideMenu(
+              backgroundColor: Colors.grey.shade700,
+              screenWidth: MediaQuery.of(context).size.width,
+              buttonClosed: Text(
+                'Compare aqui',
+              ),
+              buttonExpanded: Text(
+                'Editar opções',
+              ),
+              bodyWidget: Expanded(
+                child: ListView.builder(
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) {
+                      return Text(
+                        _items[index],
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      );
+                    }),
+              ),
+            )
+          ],
+        ));
+  }
+}
---
+++ lib/side_menu.dart
@@ -0,0 +1,109 @@
+library side_menu;
+
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatefulWidget {
+  final Color backgroundColor;
+  final double screenWidth;
+  final Widget buttonClosed;
+  final Widget buttonExpanded;
+  final Widget bodyWidget;
+
+  const SideMenu(
+      {Key key,
+      this.backgroundColor,
+      this.screenWidth,
+      this.buttonClosed,
+      this.buttonExpanded,
+      this.bodyWidget})
+      : super(key: key);
+  @override
+  _SideMenuState createState() =>
+      _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu>
+    with SingleTickerProviderStateMixin {
+  double maxWidth;
+  double buttonSize = 34;
+  double minWidth = 0;
+  bool isCollapsed = false;
+  AnimationController _animationController;
+  Animation<double> widthAnimation;
+  int currentSelectedIndex = 0;
+
+  Widget buttonWidget;
+  Widget bodyWidget;
+
+  @override
+  void initState() {
+    super.initState();
+
+    maxWidth = widget.screenWidth - buttonSize;
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    widthAnimation = Tween<double>(begin: maxWidth, end: minWidth)
+        .animate(_animationController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, _) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Center(
+                child: Container(
+                  width: buttonSize,
+                  color: Colors.transparent,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: widget.backgroundColor,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(8.0),
+                          topRight: Radius.circular(8.0),
+                        ),
+                      ),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isCollapsed = !isCollapsed;
+                            isCollapsed
+                                ? _animationController.forward()
+                                : _animationController.reverse();
+                          });
+                        },
+                        child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 8.0,
+                              horizontal: 20.0,
+                            ),
+                            child: isCollapsed
+                                ? widget.buttonClosed
+                                : widget.buttonExpanded),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                  width: widthAnimation.value,
+                  decoration: BoxDecoration(
+                    color: widget.backgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8.0),
+                      bottomLeft: Radius.circular(8.0),
+                    ),
+                  ),
+                  child: widget.bodyWidget),
+            ],
+          );
+        });
+  }
+}
